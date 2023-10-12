2a2af38e2dd6fe25fd21d1f682eeca8d45c0e50f
Inioluwa1 <adesanyanifesimi@gamil.com>
2023-10-12 11:32:40 +0100

    Clone App

--- App.js
+++ App.js
@@ -1,16 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import MainDraw from './Routes/MainDraw';
-import OtherDraw from './Routes/OtherDraw';
 import FirstStack from './Routes/FirstStack';
-import MainPage from './Pages/MainPage';
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <MainPage />
-    // </View>
     <NavigationContainer>
       <FirstStack />
     </NavigationContainer>
